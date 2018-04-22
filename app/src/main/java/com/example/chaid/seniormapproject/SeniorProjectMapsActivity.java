fee6169bc7ad70fd3f1ca297734b027f1323ab19
chaid-s <31129188+chaid-s@users.noreply.github.com>
2018-04-22 03:41:40 -0500

    Added onSearch method for search button in ui More error handling needed for invalid strings.

--- app/src/main/java/com/example/chaid/seniormapproject/SeniorProjectMapsActivity.java
+++ app/src/main/java/com/example/chaid/seniormapproject/SeniorProjectMapsActivity.java
@@ -1,14 +1,22 @@
 package com.example.chaid.seniormapproject;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
 
 public class SeniorProjectMapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -38,9 +46,36 @@ public class SeniorProjectMapsActivity extends FragmentActivity implements OnMap
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+
         // Add a marker in Sydney and move the camera
         LatLng southAL = new LatLng(30.6955253, -88.1786411);
         mMap.addMarker(new MarkerOptions().position(southAL).title("University of South Alabama"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(southAL, 14.f));
+    }
+
+    public void onSearch(View view)
+    {
+        EditText location_tf = findViewById(R.id.search);
+        String location = location_tf.getText().toString();
+        List<Address> addressList =null;
+        if(location !=null|| !location.equals(""))
+        {
+            Geocoder geocoder = new Geocoder(this);
+            try{
+                addressList = geocoder.getFromLocationName(location,1);
+
+            }
+            catch(IOException e){
+             e.printStackTrace();
+            }
+
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+
+        }
+
+
     }
 }
